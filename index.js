90c7877faa280c0847b3bb11b163beea1f4cc866
leoLuisMayaute <leomayaute85@gmail.com>
2023-01-16 09:19:29 -0500

    configuracion inicial y creacion de archivos

---
+++ index.js
@@ -0,0 +1,13 @@
+var inputText = '';
+
+function getInputText() {
+    
+    inputText = document.getElementById("input-text").value;
+}
+
+// console.log(inputText);
+// 'e', 'enter'
+// "i", "imes"
+// "o", "ober"
+// "a", "ai"
+// "u", "ufat"
