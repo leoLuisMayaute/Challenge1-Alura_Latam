26460a646c520dda897f10e47340a5b84ca6cf4d
leoLuisMayaute <leomayaute85@gmail.com>
2023-02-20 09:40:40 -0500

    almost done

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
-const botonEncriptar = document.getElementById('cipher-button');
+const botonEncriptar = document.getElementById("cipher-button");
+const botonDesencriptar = document.getElementById("uncipher-button");
+const input = document.getElementById('input-text');
+const copyButton = document.getElementById('copyToClipboard');
 
-botonEncriptar.addEventListener('click', e => {
-    console.log(e);
+
+function monstrarBotonCopy() {
+    copyButton.style.display = "block";
+}
+
+botonEncriptar.addEventListener("click", e => {
+    e.preventDefault();
+    
+    let text = input.value;
+    text.toLowerCase();
+    
+    text = text.replace(/e/g, 'enter').replace(/i/g, 'imes').replace(/o/g, 'ober').replace(/a/g, 'ai').replace(/u/g, 'ufat');
+
+    let processedText = document.getElementById('processed-text');
+
+    processedText.textContent = text;
+
+    monstrarBotonCopy();
+});
+
+botonDesencriptar.addEventListener("click", e => {
+    e.preventDefault();
+
+    let text = input.value;
+    text.toLowerCase();
+
+    text = text.replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ober/g, 'o').replace(/ai/g, 'a').replace(/ufat/g, 'u');
+
+    let processedText = document.getElementById('processed-text');
+   
+    processedText.textContent = text;
+
+    monstrarBotonCopy();
+});
+
+copyButton.addEventListener('click', e => {
+    e.preventDefault();
+    const text = document.getElementById('processed-text').innerText;
+    navigator.clipboard.writeText(text)
+        .then(() => {
+            alert("Texto copiado al portapapeles");
+        })
+        .catch((error) => {
+            alert("Hubo un error al copiar el texto");
+        });
 })
 
 
-
-
-
-
-// console.log(inputText);
-// 'e', 'enter'
-// "i", "imes"
-// "o", "ober"
-// "a", "ai"
-// "u", "ufat"
